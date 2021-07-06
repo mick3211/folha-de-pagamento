326cb008234ee23603c044a322db94a193f4e3cd
Mickael <mickaelfelizardo2008@gmail.com>
2021-07-06 19:59:03 -0300

    funcao lançar venda

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 class Adress():
     def __init__(self):
         self.cep = int()
@@ -16,28 +18,25 @@ class Adress():
         print('CEP:', self.cep)
 
 
-class Syndicate():
-    def __init__(self, name):
-        self.name = name
-
-    def Show(self):
-        print('Nome:', self.name)
-
+class venda():
+    def __init__(self, value):
+        self.time = time.asctime()
+        self.value = value
 
 class Historico():
     def __init__(self):
-        self.pontos = {}
-        self.vendas = {}
-        self.taxas = {}
+        self.pontos = list()
+        self.vendas = list()
+        self.taxas = list()
 
 
 class Person():
-    def __init__(self, name, type, paymethod, syndicate = None):
+    def __init__(self, name, type, paymethod, syndicate):
         self.name = name
         self.type = type
         self.paymethod = paymethod
         self.adress = Adress()
-        self.syndicate = Syndicate(syndicate) if syndicate is not None else None
+        self.syndicate = syndicate
         self.historico = Historico()
 
     def SetAdress(self, cep = None, numero = None, rua = None, bairro = None, cidade = None, estado = None):
@@ -48,10 +47,11 @@ class Person():
         if cidade: self.adress.cidade = cidade
         if estado: self.adress.estado = estado
 
-    def SetInfo(self, name = None, type = None, paymethod = None):
+    def SetInfo(self, name = None, type = None, paymethod = None, syndicate = None):
         if name: self.name = name
         if type: self.type = type
         if paymethod: self.paymethod = paymethod
+        if syndicate: self.syndicate = syndicate
 
     def ShowInfo(self):
         if self.type == 1: type = "Horista"
@@ -63,11 +63,14 @@ class Person():
         print('Nome:', self.name)
         print('Tipo:', type)
         print('Método de pagamento:', paymethod)
-        if self.syndicate: 
-            print('Pertence ao sindicato:')
-            self.syndicate.Show()
+        if self.syndicate: print('Pertence ao sindicato')
+        else: print('Não pertence ao sindicato')
         print('ENDEREÇO:')
         self.adress.Show()
+
+    def NewSale(self, value, date):
+        self.historico.vendas.append({'value': value, 'date': date})
+        print(self.historico.vendas)
 
 
 class Sys():
@@ -76,22 +79,26 @@ class Sys():
     CurrentEmployee = None
 
     def ShowEmployees():
-        for i, employee in enumerate(Sys.EmployeeList):
-            print(f'{i}.', employee.name)
+        if Sys.EmployeeNum > 0:
+            for i, employee in enumerate(Sys.EmployeeList):
+                print(f'{i}.', employee.name)
+            return True
+        else:
+            print('SEM FUNCIONÁRIOS CADASTRADOS')
+            return False
 
-    def SetCurrent(id = None):
-        if id == None:
-            Sys.ShowEmployees()
-            id = int(input('Selecione o funcionário: '))
-        Sys.CurrentEmployee = Sys.EmployeeList[id]  
-        return Sys.CurrentEmployee
+    def SetCurrent(id):
+        if id < Sys.EmployeeNum:
+            Sys.CurrentEmployee = Sys.EmployeeList[id]  
+            return Sys.CurrentEmployee
+        else: return False
 
     def AddEmployee(name, type, paymethod, adress, syndicate = None):
         new_employee = Person(name, type, paymethod, syndicate)
         new_employee.SetAdress(*adress)
         Sys.EmployeeList.append(new_employee)
-        Sys.SetCurrent(Sys.EmployeeNum)
         Sys.EmployeeNum += 1
+        Sys.SetCurrent(Sys.EmployeeNum - 1)
 
     def RemoveEmployee(id = None):
         if id == None: del(Sys.CurrentEmployee)
@@ -104,6 +111,7 @@ class menu():
         type = int(input('Escolha o tipo [1.Horista 2.Assalariado 3.Comissionado]: '))
         paymethod = int(input('Escolha o método de pagamento [1.Cheque pelos Correios 2.Cheque em mãos 3.Depósito bancário: '))
         adress = list()
+        syndicate = True if input('Faz parte do sindicato? [s/n]: ') in 'Ss' else False
         print('Endereço:')
         adress.append(int(input('Digite o CEP: ')))
         adress.append(int(input('Digite o Número da casa: ')))
@@ -112,58 +120,81 @@ class menu():
         adress.append(input('Digite a cidade: '))
         adress.append(input('Digite o estado: '))
 
-        Sys.AddEmployee(name, type, paymethod, adress)
+        Sys.AddEmployee(name, type, paymethod, adress, syndicate)
         print('Novo empregado adicionado:')
         Sys.CurrentEmployee.ShowInfo()
 
 
     def RemoveEmployee():
-        Sys.SetCurrent()
-        Sys.RemoveEmployee()
+        if Sys.ShowEmployees():
+            id = int(input('Selecione o funcionário a remover'))
+            if Sys.SetCurrent(id):
+                Sys.RemoveEmployee()
+            else: print('FUNCIONÁRIO INVÁLIDO')
 
 
     def EditEmployee():
         print('--------------')
-        current = Sys.SetCurrent()
-        print('EMPREGADO SELECIONADO:')
-        current.ShowInfo()
+        if Sys.ShowEmployees():
+            id = int(input('Seleione o empregado a ser editado: '))
+            current = Sys.SetCurrent(id)
+            if current != False:
+                print('EMPREGADO SELECIONADO:')
+                current.ShowInfo()
 
-        def editAdress():
-            while True:
-                print('--SELLECIONE UMA OPÇÃO--')
-                print('1.Altera CEP\n2.Alterar Rua\n3.Alterar número\n4.Alterar bairro\n5.Alterar cidade\n6.Alterar Estado\n7.Voltar')
-                op = int(input())
-                if op == 1: current.SetAdress(cep = int(input('Novo CEP: ')))
-                if op == 2: current.SetAdress(rua = input('Nova rua: '))
-                if op == 3: current.SetAdress(numero = input('Novo número: '))
-                if op == 4: current.SetAdress(bairro = input('Novo bairro: '))
-                if op == 5: current.SetAdress(cidade = input('Nova cidade: '))
-                if op == 6: current.SetAdress(estado = input('Novo Estado: '))
-                if op == 7: break
+                def editAdress():
+                    while True:
+                        print('--SELLECIONE UMA OPÇÃO--')
+                        print('1.Altera CEP\n2.Alterar Rua\n3.Alterar número\n4.Alterar bairro\n5.Alterar cidade\n6.Alterar Estado\n7.Voltar')
+                        op = int(input())
+                        if op == 1: current.SetAdress(cep = int(input('Novo CEP: ')))
+                        if op == 2: current.SetAdress(rua = input('Nova rua: '))
+                        if op == 3: current.SetAdress(numero = input('Novo número: '))
+                        if op == 4: current.SetAdress(bairro = input('Novo bairro: '))
+                        if op == 5: current.SetAdress(cidade = input('Nova cidade: '))
+                        if op == 6: current.SetAdress(estado = input('Novo Estado: '))
+                        if op == 7: break
 
-        while True:
-            print('--SELLECIONE UMA OPÇÃO--')
-            print('1.Editar nome\n2.Alterar tipo\n3.Alterar método de pagamento\n4.Editar endereço\n5.Sair')
-            option = int(input())
-            if option == 1:
-                current.SetInfo(name = input('Digite o novo nome: '))
-            if option == 2:
-                current.SetInfo(type = int(input('Escolha o tipo [1.Horista 2.Assalariado 3.Comissionado]: ')))
-            if option == 3:
-                current.SetInfo(paymethod = int(input('Escolha o método de pagamento [1.Cheque pelos Correios 2.Cheque em mãos 3.Depósito bancário: ')))
-            if option == 4: editAdress()
-            if option == 5: break
-        
-        print('--EMPREGADO EDITADO--')
-        current.ShowInfo()
+                while True:
+                    print('--SELLECIONE UMA OPÇÃO--')
+                    print('1.Editar nome\n2.Alterar tipo\n3.Alterar método de pagamento\n4.Editar endereço\n5.Alterar sindicato\n6. SAIR')
+                    option = int(input())
+                    if option == 1:
+                        current.SetInfo(name = input('Digite o novo nome: '))
+                    if option == 2:
+                        current.SetInfo(type = int(input('Escolha o tipo [1.Horista 2.Assalariado 3.Comissionado]: ')))
+                    if option == 3:
+                        current.SetInfo(paymethod = int(input('Escolha o método de pagamento [1.Cheque pelos Correios 2.Cheque em mãos 3.Depósito bancário: ')))
+                    if option == 4: editAdress()
+                    if option == 5:
+                        if current.syndicate:
+                            if input('Funcionário pertence ao sindicato, remover?[s/n]: ') in 'Ss': current.syndicate = False
+                        else: 
+                            if input('Funcionário não pertence ao sindicato, adicionar?[s/n]: ') in 'Ss': current.syndicate = True
+                    if option == 6: break
+
+                print('--EMPREGADO EDITADO--')
+                current.ShowInfo()
+            else: print('FUNCIONÁRIO INVÁLIDO')
+
+    def lancarVenda():
+        if Sys.ShowEmployees():
+            id = int(input('Selecione o funcionário para lançar a venda: '))
+            current = Sys.SetCurrent(id)
+            if current != False:
+                date = time.asctime()
+                value = float(input('Insira o valor: '))
+                current.NewSale(value, date)
+            else: print('FUNCIONÁRIO NÃO EXISTE')
 
 
 while True:
     print('--SELLECIONE UMA OPÇÃO--')
-    print('1.Adicionar empregado\n2.Remover empregado\n3.Editar empregado\n4.Listar empregados\n5.SAIR')
+    print('1.Adicionar empregado\n2.Remover empregado\n3.Editar empregado\n4.Listar empregados\n5.Lançar venda\n6. SAIR')
     option = int(input())
     if option == 1: menu.AddEmployee()
     if option == 2: menu.RemoveEmployee()
     if option == 3: menu.EditEmployee()
     if option == 4: Sys.ShowEmployees()
-    if option == 5: break
+    if option == 5: menu.lancarVenda()
+    if option == 6: break
